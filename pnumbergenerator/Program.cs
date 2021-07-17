25c10d8681fe65457543d7b16ff72e99d6b88d3b
Praktika228is <84709148+Praktika228is@users.noreply.github.com>
2021-07-18 01:06:04 +0500

    empty project

---
+++ pnumbergenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pnumbergenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
