dd4725c6d496fb4b0ddc8beb2e6634c516292116
Praktika228is <84709148+Praktika228is@users.noreply.github.com>
2021-07-18 01:51:45 +0500

    Done!

---
+++ GenerateP/GenerateP/Generator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerateP
+{
+    class Generator
+    {
+        public long dots { get; set; }
+        public bool with_logs { get; set; }
+
+        private Random rand = new Random();
+        private float PloatPnumber()
+        {
+            double x, y = 0;
+            float equation = 0;
+            float in_circle = 0;
+
+            for(int i = 0; i <= dots; i++)
+            {
+                x = rand.NextDouble();
+                y = rand.NextDouble();
+                equation = (float)(Math.Pow(x, 2) + Math.Pow(y, 2));
+                if(equation <= 1)
+                {
+                    in_circle++;
+                }
+            }
+
+            return 4 * in_circle / dots;
+        }
+
+        public float GetPnumber()
+        {
+            return PloatPnumber();
+        }
+    }
+}
--- GenerateP/GenerateP/Program.cs
+++ GenerateP/GenerateP/Program.cs
@@ -10,6 +10,15 @@ namespace GenerateP
     {
         static void Main(string[] args)
         {
+            Console.Title = "P number generator with using Monte Carlo method";
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
+            Generator generator = new Generator();
+            Console.WriteLine("Enter value of dots: ");
+            generator.dots = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Answer: {0}", generator.GetPnumber());
+            Console.ReadKey();
         }
     }
 }
